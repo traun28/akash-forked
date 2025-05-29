86bc9c561a29bf99e39a885767d1dff74fa37fce
traun28 <tarun.a@s.amity.edu>
2025-05-30 00:34:47 +0530

    added 100 lines|2.c

--- 2.c
+++ 2.c
@@ -21,4 +21,31 @@ int main() {
         printf("%d is not an Armstrong number.", num);
 
     return 0;
-}
+ calculator.c
+#include <stdio.h>
+
+int main() {
+    char operator;
+    double num1, num2;
+
+    printf("Enter operation (e.g., + - * /): ");
+    scanf(" %c", &operator);
+
+    printf("Enter two numbers: ");
+    scanf("%lf %lf", &num1, &num2);
+
+    switch (operator) {
+        case '+': printf("Result = %.2lf\n", num1 + num2); break;
+        case '-': printf("Result = %.2lf\n", num1 - num2); break;
+        case '*': printf("Result = %.2lf\n", num1 * num2); break;
+        case '/': 
+            if (num2 != 0)
+                printf("Result = %.2lf\n", num1 / num2);
+            else
+                printf("Division by zero error!\n");
+            break;
+        default: printf("Invalid operator!\n");
+    }
+
+    return 0;
+}}
